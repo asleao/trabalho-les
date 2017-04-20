0d88b3a34619728e8bbc3c0871f10aa85b0fe4f2
Andre Leao <andre.sp.leao@gmail.com>
2017-04-20 10:35:45 -0300

    Instalação do Whitenoise para os arquivos staticos

--- src/les/settings.py
+++ src/les/settings.py
@@ -132,3 +132,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
