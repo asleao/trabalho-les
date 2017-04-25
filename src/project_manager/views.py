9dec37dd25d6465fdf6bd2fdee86cde47ab219c4
Gabriel <gabriel.luiz.mb@gmail.com>
2017-04-25 15:52:01 -0300

    executado o makemigrations

--- src/project_manager/views.py
+++ src/project_manager/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
-def hello_world(request):
-    return render(request, 'base.html')
-
 def cadastro_ferramenta(request):
     return render(request, 'cadastro_ferramenta.html')
-=======
+
 def login(request):
     return render(request, 'login.html')
 
 def cadastro_usuario(request):
     return render(request, 'cadastro_usuario.html')
->>>>>>> dbd590e5c0b470740786f5bb352a179c8fe68feb
+
