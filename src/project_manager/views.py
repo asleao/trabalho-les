4510ab07910e05eb7253ab835d7769c5d454910b
Gabriel <gabriel.luiz.mb@gmail.com>
2017-04-26 11:58:35 -0300

    adicionado a view de visualizar ferramenta

--- src/project_manager/views.py
+++ src/project_manager/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
+
 def cadastro_ferramenta(request):
     return render(request, 'cadastro_ferramenta.html')
+
 
 def login(request):
     return render(request, 'login.html')
 
+
 def cadastro_usuario(request):
     return render(request, 'cadastro_usuario.html')
-
